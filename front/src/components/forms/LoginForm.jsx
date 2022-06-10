820eff79a3f37aff6ba08a7951f29581ac2265fd
davidbejenariu <72975663+davidbejenariu@users.noreply.github.com>
2022-06-11 02:41:19 +0300

    feature: hide login form inside a Modal pop-up

--- front/src/components/forms/LoginForm.jsx
+++ front/src/components/forms/LoginForm.jsx
@@ -1,43 +1,107 @@
 import React from 'react';
-import { Box, Heading, Text, HStack, VStack, FormControl, FormLabel, Input, Checkbox, Button } from '@chakra-ui/react';
+import { Box, Heading, Text, HStack, VStack, FormControl, 
+  FormLabel, Input, Checkbox, Button, FormErrorMessage } from '@chakra-ui/react';
+import { Modal, ModalOverlay, ModalContent, ModalBody, 
+  ModalCloseButton, useDisclosure } from '@chakra-ui/react';
+import { Formik, Form, Field } from 'formik';
 
-// TODO lighter font for text and checkbox
+// TODO Forgot Password?
+// TODO Remember me
 export default function RegisterForm() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = React.useRef(null)
+  
   return (
-    <Box
-      w={['full', 'md']}
-      p = {[8, 10]}
-      mt={[20, '10vh']}
-      mx='auto'
-      border={['none', '1px']}
-      borderColor={['', 'gray.300']}
-      borderRadius={10}
+    <><Button 
+      onClick={onOpen} 
+      borderRadius={10} 
+      marginRight='1vh'
     >
-      <VStack spacing={8} align='center' w='full'>
-        <VStack spacing={4} align={['flex-start', 'center']} w='full'>
-          <Heading>Login</Heading>
-          <Text fontSize='md' >Enter your email and password to login.</Text>
-        </VStack>
-        <VStack spacing={4} w='full'>
-          <FormControl>
-            <FormLabel>Email Address</FormLabel>
-            <Input variant='filled'></Input>
-          </FormControl>
-          <FormControl>
-            <FormLabel>Password</FormLabel>
-            <Input variant='filled' type='password'></Input>
-          </FormControl>
-        </VStack>
-        <HStack w='full' justify='space-between'>
-          <Checkbox>
-            <Text fontSize='sm'>Remember me</Text>
-          </Checkbox>
-          <Button variant='link' colorscheme='blue'>
-            <Text fontSize='sm'>Forgot Password?</Text>
-          </Button>
-        </HStack>
-        <Button borderRadius={10}>Login</Button>
-      </VStack>
-    </Box>
+      Login
+    </Button>
+    <Modal 
+      isOpen={isOpen} 
+      onClose={onClose}
+      isCentered
+      blockScrollOnMount={false}
+      initialFocusRef={initialRef}
+      motionPreset='slideInBottom'
+    >
+      <ModalOverlay
+        bg='blackAlpha.300'
+        backdropFilter='blur(10px) hue-rotate(90deg)'
+      />
+      <ModalContent>
+        <ModalCloseButton />
+        <ModalBody>
+          <Box
+            mx='2vh'
+            my='4vh'
+          >
+            <VStack spacing={4} align={['flex-start', 'center']} w='full'>
+              <Heading>Login</Heading>
+              <Text fontSize='md' >Enter your email and password to login.</Text>
+            </VStack>
+
+            <Box my='3vh'>
+              <Formik
+                initialValues={{email: '', password: ''}}
+                onSubmit={(values, actions) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2))
+                    actions.setSubmitting(false)
+                  }, 1000)
+                }}
+              >
+                {(props) => (
+                  <Form align='center'>
+                    <VStack spacing={4} w='full'>
+                      <Field name='email'>
+                        {({field, form}) => (
+                          <FormControl isRequired isInvalid={form.errors.name && form.touched.name}>
+                            <FormLabel>Email Address</FormLabel>
+                            <Input {...field} ref={initialRef} variant='filled'></Input>
+                            <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                      <Field name='password'>
+                        {({field, form}) => (
+                          <FormControl isRequired isInvalid={form.errors.name && form.touched.name}>
+                            <FormLabel>Password</FormLabel>
+                            <Input {...field} variant='filled' type='password'></Input>
+                            <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                    </VStack>
+
+                    <Box mt='2vh' mb='1vh'>
+                      <HStack w='full' justify='space-between'>
+                        <Checkbox>
+                          <Text fontSize='sm'>Remember me</Text>
+                        </Checkbox>
+                        <Button variant='link' colorscheme='blue'>
+                          <Text fontSize='sm'>Forgot Password?</Text>
+                        </Button>
+                      </HStack>
+                    </Box>
+                    
+                    <Button 
+                      mt='2vh'
+                      borderRadius={10}
+                      isLoading={props.isSubmitting}
+                      type='submit'
+                    >
+                      Login
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+            </Box>
+          </Box>
+        </ModalBody>
+      </ModalContent>
+    </Modal></>
   );
 }
