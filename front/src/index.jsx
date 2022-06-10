48b6edceab790f566e43d1c7b51e865ef04136b0
Luca1152 <t.luca1152@gmail.com>
2022-06-10 12:35:17 +0300

    chore: initialize Chakra.UI
    Now Chakra components can be rendered.

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ChakraProvider, extendTheme} from '@chakra-ui/react';
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const theme = extendTheme({colors});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <App/>
+      <ChakraProvider theme={theme}>
+        <App/>
+      </ChakraProvider>
     </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
