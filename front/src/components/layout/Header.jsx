493c9d2f7fc5b38c43d41fa962daed8097b6e722
davidbejenariu <72975663+davidbejenariu@users.noreply.github.com>
2022-06-11 02:41:19 +0300

    feature: hide login form inside a Modal pop-up

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Link, Button } from '@chakra-ui/react';
+import { Box, Flex, Heading, Link, Spacer } from '@chakra-ui/react';
 import ColorModeSwitcher from '../ColorModeSwitcher';
 import RegisterForm from '../forms/RegisterForm';
 import LoginForm from '../forms/LoginForm';
@@ -6,15 +6,15 @@ import LoginForm from '../forms/LoginForm';
 function Header() {
   return (
     <Box>
-      <Flex as='header' align='center' padding='8'>
+      <Flex as='header' align='center' padding='8' w='full'>
         <Heading as='h1' size='md'>
           <Link href='/'>Beat the Market</Link>
-          {/* <Button></Button> */}
-          <RegisterForm />
-          <LoginForm />
         </Heading>
+        <Spacer />
+        <LoginForm />
+        <RegisterForm />
         <Flex marginLeft='auto'>
-          <ColorModeSwitcher />
+        <ColorModeSwitcher />
         </Flex>
       </Flex>
     </Box>
