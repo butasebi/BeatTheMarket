8963da068d59410f21347a3fcc90da3f7643f462
davidbejenariu <72975663+davidbejenariu@users.noreply.github.com>
2022-06-10 17:50:22 +0300

    chore: provide register form for users

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Heading, Link } from '@chakra-ui/react';
 import ColorModeSwitcher from '../ColorModeSwitcher';
+import RegisterForm from '../forms/RegisterForm';
 
 function Header() {
   return (
@@ -7,6 +8,7 @@ function Header() {
       <Flex as='header' align='center' padding='8'>
         <Heading as='h1' size='md'>
           <Link href='/'>Beat the Market</Link>
+          <RegisterForm />
         </Heading>
         <Flex marginLeft='auto'>
           <ColorModeSwitcher />
