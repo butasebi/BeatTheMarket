a9b4e90ee6157b9497839b12b458368ddacb673a
Luca1152 <t.luca1152@gmail.com>
2022-06-11 20:57:51 +0300

    feat: use brand colors in login / register buttons
    They were gray, previously.

--- front/src/components/forms/LoginForm.jsx
+++ front/src/components/forms/LoginForm.jsx
@@ -1,107 +1,131 @@
 import React from 'react';
-import { Box, Heading, Text, HStack, VStack, FormControl, 
-  FormLabel, Input, Checkbox, Button, FormErrorMessage } from '@chakra-ui/react';
-import { Modal, ModalOverlay, ModalContent, ModalBody, 
-  ModalCloseButton, useDisclosure } from '@chakra-ui/react';
-import { Formik, Form, Field } from 'formik';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
 
 // TODO Forgot Password?
 // TODO Remember me
 export default function RegisterForm() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const initialRef = React.useRef(null)
-  
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
+
   return (
-    <><Button 
-      onClick={onOpen} 
-      borderRadius={10} 
+    <><Button
+      onClick={onOpen}
+      borderRadius={10}
       marginRight='1vh'
+      variant='ghost'
+      colorScheme='brand'
     >
       Login
     </Button>
-    <Modal 
-      isOpen={isOpen} 
-      onClose={onClose}
-      isCentered
-      blockScrollOnMount={false}
-      initialFocusRef={initialRef}
-      motionPreset='slideInBottom'
-    >
-      <ModalOverlay
-        bg='blackAlpha.300'
-        backdropFilter='blur(10px) hue-rotate(90deg)'
-      />
-      <ModalContent>
-        <ModalCloseButton />
-        <ModalBody>
-          <Box
-            mx='2vh'
-            my='4vh'
-          >
-            <VStack spacing={4} align={['flex-start', 'center']} w='full'>
-              <Heading>Login</Heading>
-              <Text fontSize='md' >Enter your email and password to login.</Text>
-            </VStack>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        blockScrollOnMount={false}
+        initialFocusRef={initialRef}
+        motionPreset='slideInBottom'
+      >
+        <ModalOverlay
+          bg='blackAlpha.300'
+          backdropFilter='blur(10px) hue-rotate(90deg)'
+        />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box
+              mx='2vh'
+              my='4vh'
+            >
+              <VStack spacing={4} align={['flex-start', 'center']} w='full'>
+                <Heading>Login</Heading>
+                <Text fontSize='md'>Enter your email and password to
+                  login.</Text>
+              </VStack>
 
-            <Box my='3vh'>
-              <Formik
-                initialValues={{email: '', password: ''}}
-                onSubmit={(values, actions) => {
-                  setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2))
-                    actions.setSubmitting(false)
-                  }, 1000)
-                }}
-              >
-                {(props) => (
-                  <Form align='center'>
-                    <VStack spacing={4} w='full'>
-                      <Field name='email'>
-                        {({field, form}) => (
-                          <FormControl isRequired isInvalid={form.errors.name && form.touched.name}>
-                            <FormLabel>Email Address</FormLabel>
-                            <Input {...field} ref={initialRef} variant='filled'></Input>
-                            <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                      <Field name='password'>
-                        {({field, form}) => (
-                          <FormControl isRequired isInvalid={form.errors.name && form.touched.name}>
-                            <FormLabel>Password</FormLabel>
-                            <Input {...field} variant='filled' type='password'></Input>
-                            <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                    </VStack>
+              <Box my='3vh'>
+                <Formik
+                  initialValues={{ email: '', password: '' }}
+                  onSubmit={(values, actions) => {
+                    setTimeout(() => {
+                      alert(JSON.stringify(values, null, 2));
+                      actions.setSubmitting(false);
+                    }, 1000);
+                  }}
+                >
+                  {(props) => (
+                    <Form align='center'>
+                      <VStack spacing={4} w='full'>
+                        <Field name='email'>
+                          {({ field, form }) => (
+                            <FormControl isRequired
+                                         isInvalid={form.errors.name &&
+                                           form.touched.name}>
+                              <FormLabel>Email Address</FormLabel>
+                              <Input {...field} ref={initialRef}
+                                     variant='filled' />
+                              <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                        <Field name='password'>
+                          {({ field, form }) => (
+                            <FormControl isRequired
+                                         isInvalid={form.errors.name &&
+                                           form.touched.name}>
+                              <FormLabel>Password</FormLabel>
+                              <Input {...field} variant='filled'
+                                     type='password' />
+                              <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                      </VStack>
 
-                    <Box mt='2vh' mb='1vh'>
-                      <HStack w='full' justify='space-between'>
-                        <Checkbox>
-                          <Text fontSize='sm'>Remember me</Text>
-                        </Checkbox>
-                        <Button variant='link' colorscheme='blue'>
-                          <Text fontSize='sm'>Forgot Password?</Text>
-                        </Button>
-                      </HStack>
-                    </Box>
-                    
-                    <Button 
-                      mt='2vh'
-                      borderRadius={10}
-                      isLoading={props.isSubmitting}
-                      type='submit'
-                    >
-                      Login
-                    </Button>
-                  </Form>
-                )}
-              </Formik>
+                      <Box mt='2vh' mb='1vh'>
+                        <HStack w='full' justify='space-between'>
+                          <Checkbox>
+                            <Text fontSize='sm'>Remember me</Text>
+                          </Checkbox>
+                          <Button variant='link' colorscheme='blue'>
+                            <Text fontSize='sm'>Forgot Password?</Text>
+                          </Button>
+                        </HStack>
+                      </Box>
+
+                      <Button
+                        mt='2vh'
+                        borderRadius={10}
+                        isLoading={props.isSubmitting}
+                        type='submit'
+                      >
+                        Login
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
+              </Box>
             </Box>
-          </Box>
-        </ModalBody>
-      </ModalContent>
-    </Modal></>
+          </ModalBody>
+        </ModalContent>
+      </Modal></>
   );
 }
--- front/src/components/forms/RegisterForm.jsx
+++ front/src/components/forms/RegisterForm.jsx
@@ -1,141 +1,179 @@
 import React from 'react';
 
-import { Heading, Text, HStack, VStack, FormControl, FormLabel, 
-  Input, Checkbox, Button, Box, FormErrorMessage, FormHelperText } from '@chakra-ui/react';
-import { Modal, ModalOverlay, ModalContent, ModalBody, 
-  ModalCloseButton, useDisclosure, InputGroup, InputRightElement } from '@chakra-ui/react';
-import { Formik, Form, Field } from 'formik';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
 
 // TODO Terms of Use and Privacy Policy?
 // TODO create account recommendation
 // TODO confirmation mail
 export default function RegisterForm() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const initialRef = React.useRef(null)
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
 
-  const [showPassword, setShowPassword] = React.useState(false)
-  const handleClick = () => setShowPassword(!showPassword)
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClick = () => setShowPassword(!showPassword);
 
   function validateEmail(value) {
-    let error
+    let error;
 
     if (!value) {
-      error = 'Email is required.'
+      error = 'Email is required.';
     } else if (!value.match(/[0-9A-Za-z._]@[0-9A-Za-z].[a-z]/g)) {
-      error = 'Email is invalid.'
+      error = 'Email is invalid.';
     }
 
-    return error
+    return error;
   }
 
   function validatePassword(value) {
-    let error
+    let error;
 
     if (!value) {
-      error = 'Password is required.'
-    } else if (!value.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,30}$/g)) {
-      error = 'Password is not strong enough.'
+      error = 'Password is required.';
+    } else if (!value.match(
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,30}$/g)) {
+      error = 'Password is not strong enough.';
     }
 
-    return error
+    return error;
   }
 
   return (
-    <><Button 
+    <><Button
       onClick={onOpen}
-      borderRadius={10} 
+      borderRadius={10}
       marginRight='1vh'
-      bgColor='gray.300'
+      colorScheme='brand'
     >
       Register
     </Button>
-    <Modal 
-      isOpen={isOpen} 
-      onClose={onClose}
-      isCentered
-      initialFocusRef={initialRef}
-      motionPreset='slideInBottom'
-    >
-      <ModalOverlay
-        bg='blackAlpha.300'
-        backdropFilter='blur(10px) hue-rotate(90deg)'
-      />
-      <ModalContent>
-        <ModalCloseButton />
-        <ModalBody>
-          <Box
-            mx='2vh'
-            my='4vh'
-          >
-            <VStack spacing={4} align={['flex-start', 'center']} w='full'>
-              <Heading>Register</Heading>
-              <Text fontSize='md'>Create your account. It's free and only takes a minute.</Text>
-            </VStack>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        initialFocusRef={initialRef}
+        motionPreset='slideInBottom'
+      >
+        <ModalOverlay
+          bg='blackAlpha.300'
+          backdropFilter='blur(10px) hue-rotate(90deg)'
+        />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box
+              mx='2vh'
+              my='4vh'
+            >
+              <VStack spacing={4} align={['flex-start', 'center']} w='full'>
+                <Heading>Register</Heading>
+                <Text fontSize='md'>Create your account. It's free and only
+                  takes a minute.</Text>
+              </VStack>
 
-            <Box my='3vh'>
-              <Formik
-                initialValues={{firstName: '', lastName: '', email: '', password: '', agreement: false}}
-                onSubmit={(values, actions) => {
-                  setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2))
-                    actions.setSubmitting(false)
-                  }, 1000)
-                }}
-              >
-                {(props) => (
-                  <Form align='center'>
-                    <VStack spacing={4} w='full'>
-                      <HStack spacing={4}>
-                        <Field name='firstName'>
-                          {({field, form}) => (
-                            <FormControl isRequired isInvalid={form.errors.firstName && form.touched.firstName}>
-                              <FormLabel htmlFor='firstName'>First Name</FormLabel>
-                              <Input {...field} ref={initialRef} variant='filled' />
-                              <FormErrorMessage>{form.errors.firstName}</FormErrorMessage>
-                            </FormControl>
-                          )}
-                        </Field>
-                        <Field name='lastName'>
-                          {({field, form}) => (
-                            <FormControl isRequired isInvalid={form.errors.lastName && form.touched.lastName}>
-                              <FormLabel htmlFor='lastName'>Last Name</FormLabel>
+              <Box my='3vh'>
+                <Formik
+                  initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    password: '',
+                    agreement: false,
+                  }}
+                  onSubmit={(values, actions) => {
+                    setTimeout(() => {
+                      alert(JSON.stringify(values, null, 2));
+                      actions.setSubmitting(false);
+                    }, 1000);
+                  }}
+                >
+                  {(props) => (
+                    <Form align='center'>
+                      <VStack spacing={4} w='full'>
+                        <HStack spacing={4}>
+                          <Field name='firstName'>
+                            {({ field, form }) => (
+                              <FormControl isRequired
+                                           isInvalid={form.errors.firstName &&
+                                             form.touched.firstName}>
+                                <FormLabel htmlFor='firstName'>First
+                                  Name</FormLabel>
+                                <Input {...field} ref={initialRef}
+                                       variant='filled' />
+                                <FormErrorMessage>{form.errors.firstName}</FormErrorMessage>
+                              </FormControl>
+                            )}
+                          </Field>
+                          <Field name='lastName'>
+                            {({ field, form }) => (
+                              <FormControl isRequired
+                                           isInvalid={form.errors.lastName &&
+                                             form.touched.lastName}>
+                                <FormLabel htmlFor='lastName'>Last
+                                  Name</FormLabel>
+                                <Input {...field} variant='filled' />
+                                <FormErrorMessage>{form.errors.lastName}</FormErrorMessage>
+                              </FormControl>
+                            )}
+                          </Field>
+                        </HStack>
+                        <Field name='email' validate={validateEmail}>
+                          {({ field, form }) => (
+                            <FormControl isInvalid={form.errors.email &&
+                              form.touched.email}>
+                              <FormLabel htmlFor='email'>Email</FormLabel>
                               <Input {...field} variant='filled' />
-                              <FormErrorMessage>{form.errors.lastName}</FormErrorMessage>
+                              <FormErrorMessage>{form.errors.email}</FormErrorMessage>
                             </FormControl>
                           )}
                         </Field>
-                      </HStack>
-                      <Field name='email' validate={validateEmail}>
-                        {({field, form}) => (
-                          <FormControl isInvalid={form.errors.email && form.touched.email}>
-                            <FormLabel htmlFor='email'>Email</FormLabel>
-                            <Input {...field} variant='filled' />
-                            <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                      <Field name='password' validate={validatePassword}>
-                        {({field, form}) => (
-                          <FormControl isInvalid={form.errors.password && form.touched.password}>
-                            <FormLabel htmlFor='password'>Password</FormLabel>
-                            <InputGroup>
-                              <Input 
-                                {...field} 
-                                variant='filled' 
-                                type={showPassword ? 'text' : 'password'} />
-                              <InputRightElement width='4.5rem'>
-                                <Button size='sm' bg='white' borderRadius={10} onClick={handleClick}>
-                                  {showPassword ? 'Hide' : 'Show'}
-                                </Button>
-                              </InputRightElement>
-                            </InputGroup>
-                            <FormHelperText align='justify'>Password should consist of at least 8 characters, 
-                              including capital letters (A-Z), digits (0-9) and special symbols (!@#$%^&*).</FormHelperText>
-                            <FormErrorMessage>{form.errors.password}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                      {/* <Field name='confirmPassword'>
+                        <Field name='password' validate={validatePassword}>
+                          {({ field, form }) => (
+                            <FormControl isInvalid={form.errors.password &&
+                              form.touched.password}>
+                              <FormLabel htmlFor='password'>Password</FormLabel>
+                              <InputGroup>
+                                <Input
+                                  {...field}
+                                  variant='filled'
+                                  type={showPassword ? 'text' : 'password'} />
+                                <InputRightElement width='4.5rem'>
+                                  <Button size='sm' bg='white' borderRadius={10}
+                                          onClick={handleClick}>
+                                    {showPassword ? 'Hide' : 'Show'}
+                                  </Button>
+                                </InputRightElement>
+                              </InputGroup>
+                              <FormHelperText align='justify'>Password should
+                                consist of at least 8 characters,
+                                including capital letters (A-Z), digits (0-9)
+                                and special symbols (!@#$%^&*).</FormHelperText>
+                              <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                        {/* <Field name='confirmPassword'>
                         {({field, form}) => (
                           <FormControl isRequired isInvalid={form.errors.name && form.touched.name}>
                             <FormLabel>Confirm Password</FormLabel>
@@ -144,36 +182,39 @@ export default function RegisterForm() {
                           </FormControl>
                         )}
                       </Field> */}
-                    </VStack>
+                      </VStack>
 
-                    <Box mt='2vh' mb='1vh' align='left'>
-                      <Field name='agreement'>
-                        {({field, form}) => (
-                          <FormControl isRequired isInvalid={form.errors.agreement && form.touched.agreement}>
-                            <Checkbox {...field}>
-                              <Text fontSize='sm'>I accept the Terms of Use & Privacy Policy</Text>
-                            </Checkbox>
-                            <FormErrorMessage>{form.errors.agreement}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                    </Box>
+                      <Box mt='2vh' mb='1vh' align='left'>
+                        <Field name='agreement'>
+                          {({ field, form }) => (
+                            <FormControl isRequired
+                                         isInvalid={form.errors.agreement &&
+                                           form.touched.agreement}>
+                              <Checkbox {...field}>
+                                <Text fontSize='sm'>I accept the Terms of Use &
+                                  Privacy Policy</Text>
+                              </Checkbox>
+                              <FormErrorMessage>{form.errors.agreement}</FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                      </Box>
 
-                    <Button 
-                      mt='2vh'
-                      borderRadius={10}
-                      isLoading={props.isSubmitting}
-                      type='submit'
-                    >
-                      Create Account
-                    </Button>
-                  </Form>
-                )}
-              </Formik>
+                      <Button
+                        mt='2vh'
+                        borderRadius={10}
+                        isLoading={props.isSubmitting}
+                        type='submit'
+                      >
+                        Create Account
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
+              </Box>
             </Box>
-          </Box>
-        </ModalBody>
-      </ModalContent>
-    </Modal></>
+          </ModalBody>
+        </ModalContent>
+      </Modal></>
   );
 }
