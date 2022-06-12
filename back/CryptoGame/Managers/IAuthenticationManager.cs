01d0ed683b6ba50ce45aca7331f65a2495a3223e
Ionut Anghelina <ionutmariananghelina@gmail.com>
2022-06-13 01:55:47 +0300

    added necessary models

---
+++ back/CryptoGame/Managers/IAuthenticationManager.cs
@@ -0,0 +1,14 @@
+﻿using CryptoGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoGame.Managers
+{
+    public interface IAuthenticationManager
+    {
+        Task Signup(SignupUserModel signupUserModel);
+        Task<TokenModel> Login(LoginUserModel loginUserModel);
+    }
+}
---
+++ back/CryptoGame/Models/SignupUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proiect_DAW.Models
+{
+    public class SignupUserModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RoleId { get; set; }
+    }
+}
