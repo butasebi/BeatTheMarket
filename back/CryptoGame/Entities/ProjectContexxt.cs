35b3994c57b6e64cc79e5045b1fa829e60a3e8a2
Ionut Anghelina <ionutmariananghelina@gmail.com>
2022-06-13 01:50:25 +0300

    added project context

---
+++ back/CryptoGame/Entities/ProjectContexxt.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoGame.Entities
+{
+    public class ProjectContext : IdentityDbContext<User, Role, string, IdentityUserClaim<string>,
+       UserRole, IdentityUserLogin<string>, IdentityRoleClaim<string>, IdentityUserToken<string>>
+    {
+
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options) { }
+
+        /**protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+                .UseSqlServer("Server=(localdb)\\ProjectsV13;Initial Catalog=Items;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }**/
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
