2ddb4d85ed54aac6f1a02f50bfc5b527bdb929e9
Luca1152 <t.luca1152@gmail.com>
2022-06-12 11:50:20 +0300

    feat: make 'Sell' button change to 'Buy' when clicked
    And back.

--- front/src/components/game/Game.jsx
+++ front/src/components/game/Game.jsx
@@ -1,16 +1,36 @@
 import { Box, Button, Center } from '@chakra-ui/react';
 import GameChart from './GameChart';
 import { generateStockData } from './dataGenerator';
+import { useState } from 'react';
+
+function BuySellButton(props) {
+  const { isSelling, setIsSelling } = props;
+
+  const buttonText = isSelling ? 'Sell' : 'Buy';
+  const variant = isSelling ? 'outline' : 'solid';
+  const onClick = () => {
+    setIsSelling(!isSelling);
+  };
+
+  return (
+    <Button colorScheme='brand' width='4rem' ml='4.7rem' variant={variant}
+            onClick={onClick}>
+      {buttonText}
+    </Button>
+  );
+}
 
 function Game() {
+  const [isSelling, setIsSelling] = useState(true);
+
   const timeInterval = 'day';
   const data = generateStockData(timeInterval);
 
   return (
     <Box>
       <GameChart rawData={data} dataTimeInterval={timeInterval} />
-      <Center mt='1rem'>
-        <Button colorScheme='brand' ml='4.5rem'>Sell</Button>
+      <Center mt='3'>
+        <BuySellButton isSelling={isSelling} setIsSelling={setIsSelling} />
       </Center>
     </Box>
   );
