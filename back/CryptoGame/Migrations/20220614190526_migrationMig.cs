4d3e4a3a8b6eb93cba6abbb0ae2651a89806c455
Ionut Anghelina <ionutmariananghelina@gmail.com>
2022-06-14 22:28:16 +0300

    changed the structure for username

---
+++ back/CryptoGame/Migrations/20220614190526_migrationMig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CryptoGame.Migrations
+{
+    public partial class migrationMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
--- back/CryptoGame/Models/LoginUserModel.cs
+++ back/CryptoGame/Models/LoginUserModel.cs
@@ -9,5 +9,9 @@ namespace CryptoGame.Models
     {
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
     }
 }
