22948fb5cf85ce6d5565c51b08e7ae5d1658b9a6
butasebi <butasebi@yahoo.com>
2022-06-14 10:35:54 +0300

    Created leaderboard controller

---
+++ back/CryptoGame/Controllers/LeaderboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoGame.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
