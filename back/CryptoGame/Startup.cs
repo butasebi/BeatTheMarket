d0c0de1c26c6ecda00be1269f885f370f5c6e5b0
butasebi <butasebi@yahoo.com>
2022-06-14 14:13:17 +0300

    Removed space

--- back/CryptoGame/Startup.cs
+++ back/CryptoGame/Startup.cs
@@ -73,7 +73,6 @@ namespace CryptoGame
                     }
                 });
             });
-            
             services.AddDbContext<CryptoGameContext>(options => options
                 .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
                 .UseSqlServer("Server = (localdb)\\MSSQLLocalDB;Initial Catalog=CryptoGame;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
